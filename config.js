3e30e87d8c391b0e4f143d4935d68996bcfa54fe
Mika Lindqvist <postmaster@raasu.org>
2021-04-01 20:01:01 +0200

    [config] Update location of official pool as it changed data center.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const transactionExplorer = "?hash={id}#transaction";
 
 // poolList ["id","visible", "url", "api", "enabled"]
 const poolList = [
-    {"id":1,"visible":true,"url":"https://pool.talleo.org","api":"https://pool.talleo.org:7112","status":true,"flag":"FR","loc":"FRANCE"},
+    {"id":1,"visible":true,"url":"https://pool.talleo.org","api":"https://pool.talleo.org:7112","status":true,"flag":"DE","loc":"GERMANY"},
     {"id":2,"visible":true,"url":"https://pool.raasu.org","api":"https://pool.raasu.org:7119","status":true,"flag":"FI","loc":"FINLAND"},
     {"id":3,"visible":true,"url":"https://tlo.cryptonote.club","api":"https://tlo.cryptonote.club:8199","status":true,"flag":"DE","loc":"GERMANY"},
 ];
